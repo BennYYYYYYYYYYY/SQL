65e4543836d4fb0ec4aac571ada43d3873560096
HUANG HSIANG <129297292+BennYYYYYYYYYYY@users.noreply.github.com>
2024-08-11 02:27:28 +0800

    Update 2. Data Definition Language.sql

--- 進階SQL五十道練習/2. Data Definition Language.sql
+++ 進階SQL五十道練習/2. Data Definition Language.sql
@@ -3,22 +3,15 @@ CREATE DATABASE cloned_covid19 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 SHOW DATABASES;
 
 
-# 在 cloned_covid19 資料庫中，建立 locations 資料表
-CREATE TABLE cloned_covid19.locations (
+# 建立新資料表
+CREATE TABLE cloned_imdb.movies(
 	id int UNSIGNED,
-	country_name varchar(200),
-	iso2 char(2),
-	iso3 char(3),
-	longitude float,
-	latitude float
-);
-
-
-# 在 cloned_covid19 資料庫中，建立 calendars 資料表
-CREATE TABLE cloned_covid19.calendars(
-	id int unsigned,
-	recorded_on date
-);
+	title varchar(200),
+	rating float,
+	release_year YEAR,		# year: yyyy
+	release_date date,		# date: 'YYYY-MM-DD'
+	release_time time,		# time: 'hh:mm:ss'
+)
 
 
 # 在 cloned_covid19 資料庫中，建立 accumulative_cases 資料表
@@ -29,6 +22,26 @@ CREATE TABLE cloned_covid19.accumulative_cases(
 	confirmed bigint,
 	deaths bigint
 );
+
+
+# 在已經存在的資料表中新增一個名為 title 的欄位
+ALTER TABLE cloned_imdb.movies ADD title varchar(200);
+
+
+# 從資料表中刪除名為 title 的欄位
+ALTER TABLE cloned_imdb.movies DROP title;
+
+
+# 將資料表中的 id 欄位重新命名為 movie_id
+ALTER TABLE cloned_imdb.movies RENAME COLUMN id TO movie_id;
+
+
+# 將資料表中的 movie_id 欄位修改為 tinyint unsigned 類型
+ALTER TABLE cloned_imdb.movies MODIFY COLUMN movie_id tinyint unsigned;
+
+
+# 清空資料表中的所有數據
+TRUNCATE TABLE cloned_imdb.movies;
 
 
 # 在 covid19 資料庫中，篩選 accumulative_cases.calendar_id = 1164 建立 accumulative_cases_20230309 檢視表
@@ -84,86 +97,4 @@ SELECT locations_twn_jpn.country_name,
 FROM covid19.accumulative_cases_20230309
 RIGHT JOIN covid19.locations_twn_jpn
 ON accumulative_cases_20230309.location_id = locations_twn_jpn.id
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
